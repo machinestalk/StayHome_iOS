cfdddbce6682546e62fd4523434d88134ff144e1
Aymen HECHMI <aymen.hechmi@machinestalk.com>
2020-04-04 12:25:57 +0100

    adding views

---
+++ lockDown/lockDown/SignUp/WelcomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  WelcomeViewController.swift
+//  lockDown
+//
+//  Created by Aymen HECHMI on 04/04/2020.
+//  Copyright © 2020 Ahmed Mh. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: BaseController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
