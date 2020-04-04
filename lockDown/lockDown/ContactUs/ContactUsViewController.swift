99f1775d3a147d80d89f52255c74b65a37b11bfc
Aymen HECHMI <aymen.hechmi@machinestalk.com>
2020-04-04 22:15:56 +0100

    Progress on contact us and emergency

---
+++ lockDown/lockDown/ContactUs/ContactUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactUsViewController.swift
+//  lockDown
+//
+//  Created by Aymen HECHMI on 04/04/2020.
+//  Copyright © 2020 Ahmed Mh. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
