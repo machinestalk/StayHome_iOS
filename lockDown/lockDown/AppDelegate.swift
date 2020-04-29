78615a049de243e50b98ebe7b056f002d6a12e31
Ahmed Mh <ahmed.mahouchi@machinestalk.com>
2020-04-29 16:47:18 +0100

    remove subscribing to topic

--- lockDown/lockDown/AppDelegate.swift
+++ lockDown/lockDown/AppDelegate.swift
@@ -87,11 +87,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.tintColor = UIColor.white
         self.window?.makeKeyAndVisible()
         GMSServices.provideAPIKey(LockDown.GoogleMaps.key)
-        if Messaging.messaging().fcmToken != nil {
+       /* if Messaging.messaging().fcmToken != nil {
             Messaging.messaging().subscribe(toTopic: "stayhome") { error in
                    print("Subscribed to AllDevices topic")
             }
-        }
+        }*/
         
         
         return true
@@ -268,12 +268,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         completionHandler(UIBackgroundFetchResult.newData)
     }
-    func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
-         Messaging.messaging().subscribe(toTopic: "stayhome") { error in
-                   print("Subscribed to AllDevices topic")
-               }
-    }
-    
+   
     // [END receive_message]
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print("Unable to register for remote notifications: \(error.localizedDescription)")
@@ -284,9 +279,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // the FCM registration token.
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         print("APNs token retrieved: \(deviceToken)")
-        Messaging.messaging().subscribe(toTopic: "stayhome") { error in
+       /* Messaging.messaging().subscribe(toTopic: "stayhome") { error in
                print("Subscribed to AllDevices topic")
-        }
+        }*/
         // With swizzling disabled you must set the APNs token here.
         // Messaging.messaging().apnsToken = deviceToken
     }
@@ -418,9 +413,9 @@ extension AppDelegate : MessagingDelegate {
         let dataDict:[String: String] = ["token": fcmToken]
         NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
         
-        Messaging.messaging().subscribe(toTopic: "stayhome") { error in
+       /* Messaging.messaging().subscribe(toTopic: "stayhome") { error in
             print("Subscribed to AllDevices topic")
-        }
+        }*/
        
         // TODO: If necessary send token to application server.
         // Note: This callback is fired at each app startup and whenever a new token is generated.
